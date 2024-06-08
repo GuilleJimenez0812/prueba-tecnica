5ab86a1a5bc82f2326f118fcb6bf0d9faa2374e6
GuilleJimenez0812 <135547334+GuilleJimenez0812@users.noreply.github.com>
2024-06-08 12:16:08 -0400

    lodash

---
+++ src/middlewares/index.ts
@@ -0,0 +1,43 @@
+import express from 'express'
+import { get, merge } from 'lodash'
+import { getuserBySessionToken } from '../db/user'
+
+export const isAuthenticated = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    try {
+        const sessionToken = req.cookies['GUILLERMO-AUTH']
+
+        if (!sessionToken) {
+            return res.sendStatus(403)
+        }
+
+        const existingUser = await getuserBySessionToken(sessionToken)
+
+        if (!existingUser) {
+            return res.sendStatus(403)
+        }
+
+        merge(req, {identity: existingUser})
+    } catch (error) {
+        console.log(error)
+        return res.sendStatus(400)
+    }
+}
+
+export const isOwner = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    try{
+        const { id} = req.params
+        const currentUserId = get(req, 'identity._id') as string
+
+        if (!currentUserId) {
+            return res.sendStatus(403)
+        }
+        if (currentUserId.toString() !== id) {
+            return res.sendStatus(403)
+        }
+
+        next()
+    }catch (err) {
+        console.log(err)
+        return res.sendStatus(400)
+    }
+}
