552cbe7eb47a6d62b9c596d29f601c7293987911
GuilleJimenez0812 <135547334+GuilleJimenez0812@users.noreply.github.com>
2024-06-08 10:47:32 -0400

    CRUD de user con mongoose

--- src/db/user.ts
+++ src/db/user.ts
@@ -11,3 +11,13 @@ const UserSchema = new mongoose.Schema({
 })
 
 export const UserModel = mongoose.model('User', UserSchema)
+
+export const getUser = () => UserModel.find()
+export const getUserByEmail = (email: string) => UserModel.findOne({email})
+export const getuserBySessionToken = (sessionToken: string) => UserModel.findOne({ 
+    'authentication.sessionToken': sessionToken
+})
+export const getUserById = (id: string) => UserModel.findById(id)
+export const createUser = (value: Record<string, any>) => new UserModel(value).save().then((user) => user.toObject())
+export const deleteuserById = (id: string) => UserModel.findOneAndDelete({_id: id })
+export const updateUserById = (id: string, values: Record<string, any>) => UserModel.findByIdAndUpdate(id, values)
