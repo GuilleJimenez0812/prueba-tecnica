3148978de4009a57f41e15ce7380b2607a2cf44a
GuilleJimenez0812 <135547334+GuilleJimenez0812@users.noreply.github.com>
2024-06-08 12:49:03 -0400

    JWT

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -6,3 +6,5 @@ export const random = () => crypto.randomBytes(128).toString('base64')
 export const authentication = (salt: string, password: string) => {
     return crypto.createHmac('sha256', [salt, password].join('/')).update(SECRET).digest('hex')
 }
+
+export const secret = 'soy-una-prueba'
